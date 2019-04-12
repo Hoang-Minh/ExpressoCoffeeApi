f90019b5cf77e5fcc13fce25802421b5f92487e7
Hoang-Minh <quataoxanh83@gmail.com>
2019-04-12 00:06:33 -0700

    Add Migration

--- ExpressoApi/Startup.cs
+++ ExpressoApi/Startup.cs
@@ -1,18 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ExpressoApi.Service;
+﻿using ExpressoApi.Service;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace ExpressoApi
 {
@@ -35,7 +27,7 @@ namespace ExpressoApi
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ExpressoDbContext expressoDbContext)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,7 +38,7 @@ namespace ExpressoApi
                 app.UseHsts();
             }
 
-            expressoDbContext.Database.EnsureCreated();
+            
             app.UseHttpsRedirection();
             app.UseMvc();
         }
